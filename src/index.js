8def85f5ce6731cef9b3151d64c23ac51835f5b5
Pavel Sapachev <pavel@sapachev.com>
2021-06-14 20:21:04 +0300

    task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 // Please run your solution from this file
 
+import { prepareData, renderData } from './solution';
+
 console.log("Hello from %csrc/index.js", "font-weight:bold");
+
+fetch('https://api.spacexdata.com/v3/launches/past')
+  .then(response => {
+    if (response.ok) {
+      return response.json()
+    }
+    throw new Error('Failed to fetch data');
+  })
+  .then(data => {
+    renderData(prepareData(data));
+  })
+  .catch(err => {
+    document.getElementById("out").innerText = `Error: ${err.message}`;
+  });
--- src/solution.js
+++ src/solution.js
@@ -1,8 +1,74 @@
 // Please implement your solution in this file
 
-const noop = () => {};
+const filter = (data, path, filterFn) => {
+  if (!data.length) {
+    throw new Error(`Empty array to filter`);
+  }
+
+  return data.filter(obj => {
+    const propValue = extractPropValue(obj, path);
+    return filterFn(propValue);
+  });
+}
+
+const immutableSort = (data, path) => {
+  if (!data.length) {
+    throw new Error(`Empty array to sort`);
+  }
+
+  return [...data].sort((a, b) => {
+    const aValue = extractPropValue(a, path);
+    const bValue = extractPropValue(b, path);
+    if ((aValue.length || aValue) < (bValue.length || bValue)) {
+      return 1;
+    }
+    if ((aValue.length || aValue) > (bValue.length || bValue)) {
+      return -1;
+    }
+    return 0;
+  });
+}
+
+const map = (data) => {
+  if (!data.length) {
+    throw new Error(`Empty array to map`);
+  }
+
+  return data.map(({ flight_number, mission_name, rocket }) => {
+    const payloads_count = extractPropValue(rocket, 'second_stage:payloads').length;
+    return {
+      flight_number,
+      mission_name,
+      payloads_count
+    }
+  });
+}
+
+const extractPropValue = (obj, path) => {
+  return path.split(':').reduce((obj, field) => {
+    if (!obj[field]) {
+      throw new Error(`Unknown path parameter "${field}" in "${path}"`);
+    }
+    return obj[field];
+  }, obj);
+}
+
+function prepareData(data, year = 2018, customer = 'NASA') {
+  data = filter(data, 'launch_year', (launchYear) => launchYear === String(year));
+  data = filter(data, 'rocket:second_stage:payloads', (payloads) => {
+    const re = new RegExp(`^${customer}.*$`, '');
+    return payloads.some(({ customers }) => customers.some(c => re.test(c)));
+  });
+  data = immutableSort(data, 'flight_number', false);
+  data = immutableSort(data, 'rocket:second_stage:payloads', false);
+  return map(data);
+}
+
+function renderData(data) {
+  document.getElementById("out").innerHTML = JSON.stringify(data, null, 2);
+}
 
 module.exports = {
-  prepareData: noop,
-  renderData: noop
+  prepareData,
+  renderData
 };
